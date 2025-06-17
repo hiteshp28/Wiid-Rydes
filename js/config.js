81ac8eaccde12e47e770b7cf49ecc9c8c5eb5174
Hitesh Pawar <135259276+hiteshp28@users.noreply.github.com>
2025-06-17 19:44:26 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_WTXWTTnQJ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '10r9j0brocd0t965rn6j58olfj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '40s94pn116l1q9d8fm8iu86ttj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
